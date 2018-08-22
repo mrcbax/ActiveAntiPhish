d471f8978eef4a8d9ccd423033a7a3c70532f6c0
Chad Baxter <cbaxter@mail.umw.edu>
2018-08-21 20:28:35 -0500

    dang single quotes

--- misc/process.py
+++ misc/process.py
@@ -25,7 +25,7 @@ for f in files:
 
 #Unzip all
 rootPath = r"./"
-pattern = '*.zip'
+pattern = "*.zip"
 for root, dirs, files in os.walk(rootPath):
     for filename in fnmatch.filter(files, pattern):
         print(os.path.join(root, filename))
